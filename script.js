2c5d4e3f084420b6ac22d5e167191cfa75a764da
Bayron David <93720978+BayronDavid@users.noreply.github.com>
2022-03-06 19:08:33 -0500

    Maquetado HTML

---
+++ script.js
@@ -0,0 +1,33 @@
+var img = new Image();
+img.src = "./descarga.png";
+img.onload = function () {
+  draw(this);
+};
+
+function draw(img) {
+  var canvas = document.getElementById("canvas");
+  var ctx = canvas.getContext("2d");
+  ctx.drawImage(img, 0, 0);
+  img.style.display = "none";
+  var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  var data = imageData.data;
+
+  var invert = function () {
+    for (var i = 0; i < data.length; i += 4) {
+      data[i] = 255 - data[i]; // red
+      data[i + 1] = 255 - data[i + 1]; // green
+      data[i + 2] = 255 - data[i + 2]; // blue
+    }
+    ctx.putImageData(imageData, 0, 0);
+  };
+
+  var grayscale = function () {
+    for (var i = 0; i < data.length; i += 4) {
+      var avg = (data[i] + data[i + 1] + data[i + 2]) / 3;
+      data[i] = avg; // red
+      data[i + 1] = avg; // green
+      data[i + 2] = avg; // blue
+    }
+    ctx.putImageData(imageData, 0, 0);
+  };
+}
